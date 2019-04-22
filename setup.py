af513c148404f0cc6ec27a1411f40504b0da7a0e
nex <andresgu@gmail.com>
2019-04-22 14:56:50 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'datosgobmx',
   packages = ['datosgobmx'], # this must be the same as the name above
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Connect to api.datos.gob.mx',
   author = 'Leo Castaneda',
   author_email = 'leonel.castaneda@datos.mx',
   url = 'https://github.com/mxabierto/api_python_client',
-  download_url = 'https://github.com/mxabierto/api_python_client/archive/0.1.1.tar.gz',
+  download_url = 'https://github.com/mxabierto/api_python_client/archive/0.1.2.tar.gz',
   keywords = ['ckan', 'dgm', 'open data'],
   classifiers = []
 )
